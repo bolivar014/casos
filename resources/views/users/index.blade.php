8bc0712bb55d2a9f41e1c26ca3bd75aa8a626d86
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-07-26 16:29:14 -0500

    integrando front | vista principal - usuarios

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.base')
+    @section('title-content')
+        <h1 class="m-0"><i class="fas fa-network-wired"></i> Usuarios</h1>
+    @endsection
+    @section('content')
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="row">
+                                    <div class="col-6">
+                                        <h3 class="card-title">Lista de usuarios</h3>
+                                    </div>
+                                    <div class="col-6" style="right: 0!important; position: absolute; display:flex; justify-content: end;">
+                                        <a href="{{ url('/users/create') }}" class="btn btn-success"><i class="fas fa-plus"></i> Agregar usuario</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th style="width: 10px">#</th>
+                                            <th>Nombre cliente</th>
+                                            <th>Fecha Creación</th>
+                                            <th style="width: 70px">&nbsp;&nbsp;Opciones&nbsp;&nbsp;</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                      
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                            
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </div>
+        </section>
+    @endsection
