b69ef1510f404edd2218be4eca9357847dc4aa9f
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-07-28 07:35:33 -0500

    ajustando front | vista valor TRM - CO

--- resources/views/apis/show.blade.php
+++ resources/views/apis/show.blade.php
@@ -1,12 +1,21 @@
 @extends('layouts.base')
     @section('title-content')
-        <h1 class="m-0"><i class="fas fa-ticket-alt"></i> Casos</h1>
+        <h1 class="m-0"><i class="fas fa-solid fa-dollar-sign"></i> Información TRM</h1>
     @endsection
     @section('content')
         <section class="content">
             <div class="container-fluid">
                 <div class="row">
-                    el TRM para hoy del pais de colombia, es: $<span id="spanTrmCo" name="spanTrmCo"></span>
+                    <div class="col-md-12">
+                        <div class="info-box mb-3 bg-warning">
+                            <!--<span class="info-box-icon"></span>-->
+                            <i class="nav-icon fas fa-chart-bar info-box-icon"></i> 
+                            <div class="info-box-content">
+                                <span class="info-box-text">Valor TRM hoy <?php echo date('Y-m-d'); ?></span>
+                                <span id="spanTrmCo" name="spanTrmCo" class="info-box-number"></span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
@@ -21,7 +30,7 @@
             console.log('success');
             console.log(response);
 
-            $('#spanTrmCo').html(response);
+            $('#spanTrmCo').html('$' + response);
         }).fail(function(err) {
             console.log('error');
             console.log(err);
